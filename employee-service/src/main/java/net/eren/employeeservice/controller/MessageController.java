78a9d1c8ed951b8cd470e53bd4f618d05ae75f2e
isa eren <isaeren.ie@gmail.om>
2023-03-31 00:05:15 +0300

    include

--- employee-service/src/main/java/net/eren/employeeservice/controller/MessageController.java
+++ employee-service/src/main/java/net/eren/employeeservice/controller/MessageController.java
@@ -5,16 +5,16 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RefreshScope
 @RestController
 public class MessageController {
+
     @Value("${spring.boot.message}")
     private String message;
 
     @GetMapping("/users/message")
-    public String getMessage() {
+    public String message(){
         return message;
     }
-
 }
+
